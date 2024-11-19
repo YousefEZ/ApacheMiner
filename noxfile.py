752f98ecd25dfbba9c8e3ab4271c9234305b4744
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-11-19 22:18:50 +0000

    :sparkles: add nox tests

---
+++ noxfile.py
@@ -0,0 +1,19 @@
+import nox
+
+
+@nox.session
+def tests(session):
+    session.install("pytest")
+    session.run("pytest")
+
+
+@nox.session
+def lint(session):
+    session.install("flake8")
+    session.run("flake8", "src", "tests")
+
+
+@nox.session
+def formatting(session):
+    session.install("black")
+    session.run("black", "src", "tests")
