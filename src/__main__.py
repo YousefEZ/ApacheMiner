180588081f44b208001132eeda6382b7b5a0a997
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-11-20 23:46:11 +0000

    :sparkles: add --no-dormant and --no-incubating flags

--- src/__main__.py
+++ src/__main__.py
@@ -1,10 +1,24 @@
 import csv
+import re
 
 import click
 import rich.progress
+import rich.theme
 
 from . import apache_list, github
 from .driver import generate_driver
+
+HEADER = ["name", "repository"]
+
+theme = rich.theme.Theme(
+    {
+        "info": "cyan",
+        "danger": "bold red",
+        "warning": "bold yellow",
+        "success": "bold green",
+    }
+)
+console = rich.console.Console(theme=theme)
 
 
 @click.group()
@@ -18,28 +32,46 @@ def fetch(): ...
 @fetch.command()
 @click.argument("output", type=click.Path())
 @click.option("--no-attic", is_flag=True, help="Exclude attic projects")
+@click.option("--no-incubating", is_flag=True, help="Exclude incubating projects")
+@click.option("--no-dormant", is_flag=True, help="Exclude incubating projects")
 def apache(
     output: str,
     no_attic: bool = False,
+    no_incubating: bool = False,
+    no_dormant: bool = False,
 ):
     driver = generate_driver()
     project_list = apache_list.retrieve_project_list(driver)
     if no_attic:
         project_list = [project for project in project_list if not project.in_attic]
+    if no_incubating:
+        project_list = [project for project in project_list if not project.in_incubator]
+    if no_dormant:
+        project_list = [project for project in project_list if not project.is_dormant]
 
     with open(output, "w") as f:
         writer = csv.writer(f)
-        writer.writerow(["name", "repository", "project"])
-        with rich.progress.Progress() as progress:
-            task = progress.add_task("Fetching projects...", total=len(project_list))
+        writer.writerow(HEADER)
+        with rich.progress.Progress(console=console) as progress:
+            task = progress.add_task(
+                ":rocket: Fetching projects...", total=len(project_list)
+            )
             for project in project_list:
-                progress.console.print(f"Fetching GitHub Repository for {project}")
+                progress.console.print(
+                    f":mag_right: Fetching GitHub Repository for {project.name}",
+                    emoji=True,
+                )
                 github_repository = project.fetch_github_project(driver)
                 if github_repository:
                     writer.writerow([project.name, github_repository.url])
-                    progress.console.print(f"|--> Repository: {github_repository.url}")
+                    progress.console.print(
+                        "|--> :heavy_check_mark:  Repository [success]Found[/success]",
+                        emoji=True,
+                    )
                 else:
-                    progress.console.print("|--> No Repository Found")
+                    progress.console.print(
+                        "|--> :x: Repository [danger]Not Found[/danger]", emoji=True
+                    )
                 progress.advance(task)
 
     driver.quit()
@@ -50,15 +82,24 @@ def apache(
 @click.argument("output", type=click.Path())
 def github_list(url: str, output: str):
     console = rich.console.Console()
-    console.print(f"Fetching GitHub Repository for {url}")
-    driver = generate_driver()
-    project_list = github.retrieve_project_list(url, driver)
+
+    regex = r"https://github\.com/.+/repositories.*"
+    if not re.match(regex, url):
+        console.print(":x: Invalid GitHub URL", emoji=True, style="bold red")
+        return
+    with console.status(f":mag_right: Fetching GitHub Repository for {url}"):
+        driver = generate_driver()
+        project_list = github.retrieve_project_list(url, driver)
+
+    console.print(f"Found [cyan]{len(project_list)}[/cyan] projects")
 
     with open(output, "w") as f:
         with rich.progress.Progress() as progress:
-            task = progress.add_task("Writing projects...", total=len(project_list))
+            task = progress.add_task(
+                ":pencil2:  Writing projects...", emoji=True, total=len(project_list)
+            )
             writer = csv.writer(f)
-            writer.writerow(["name", "repository"])
+            writer.writerow(HEADER)
             for project in project_list:
                 writer.writerow([project.name, project.url])
                 progress.advance(task)
