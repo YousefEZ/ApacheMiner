79085e4cd1aa7bbf9ad276f04544d89e48f32208
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-11-29 23:28:35 +0000

    :sparkles: revamp driller to work with transactions

--- src/driller.py
+++ src/driller.py
@@ -8,6 +8,14 @@ import rich.progress
 from bs4 import BeautifulSoup
 from urllib3 import request
 
+modification_map: dict[pydriller.ModificationType, str] = {
+    pydriller.ModificationType.ADD: "A",
+    pydriller.ModificationType.COPY: "C",
+    pydriller.ModificationType.DELETE: "D",
+    pydriller.ModificationType.MODIFY: "M",
+    pydriller.ModificationType.RENAME: "R",
+}
+
 
 def fetch_number_of_commits(url: str) -> Optional[int]:
     response = request(method="GET", url=url)
@@ -35,21 +43,34 @@ def fetch_number_of_commits(url: str) -> Optional[int]:
 
 
 def drill_repository(
-    url: str, output_file: str, progress: rich.progress.Progress
+    url: str, output_file: str, progress: rich.progress.Progress, delimiter: str = "|"
 ) -> None:
     commit_count = fetch_number_of_commits(url)
     with open(output_file, "w") as f:
         task = progress.add_task(
             f"Fetching commits for [cyan]{url}[/cyan]", total=commit_count
         )
-        writer = csv.writer(f)
+        writer = csv.DictWriter(f, fieldnames=["hash", "file", "modification_type"])
         for commit in pydriller.Repository(url).traverse_commits():
             progress.advance(task)
-            writer.writerow(
-                [
-                    commit.hash,
-                    "|".join(file.filename for file in commit.modified_files),
-                ]
-            )
+            for file in commit.modified_files:
+                if file.change_type == pydriller.ModificationType.RENAME:
+                    writer.writerow(
+                        {
+                            "hash": commit.hash,
+                            "file": f"{file.old_path}{delimiter}{file.new_path}",
+                            "modification_type": modification_map[
+                                pydriller.ModificationType.RENAME
+                            ],
+                        }
+                    )
+                else:
+                    writer.writerow(
+                        {
+                            "hash": commit.hash,
+                            "file": file.filename,
+                            "modification_type": modification_map[file.change_type],
+                        }
+                    )
 
         progress.tasks[task].visible = False
