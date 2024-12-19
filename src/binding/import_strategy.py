a0653dc8954ea761b6d139bfdd296da9a48d63ed
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-12-19 22:39:53 +0300

    :sparkles: add binding strategy by using the import statements

---
+++ src/binding/import_strategy.py
@@ -0,0 +1,73 @@
+from functools import lru_cache
+from typing import Optional
+import os
+
+import rich.progress
+
+from src.binding.strategy import BindingStrategy
+from src.binding.graph import Graph
+from src.binding.file_types import SourceFile, TestFile, JavaFile
+
+
+class ImportStrategy(BindingStrategy):
+    """This strategy of binding is based on the import statements in the java files."""
+
+    def __init__(
+        self, source_files: set[SourceFile], test_files: set[TestFile]
+    ) -> None:
+        self._source_files = source_files
+        self._test_files = test_files
+
+    def import_name_of(self, java_file: JavaFile) -> str:
+        directories = java_file.abs_path.split(os.path.sep)
+        for idx, subdirectory in enumerate(directories):
+            if subdirectory == "java":
+                break
+        else:
+            raise ValueError(f"Cannot find java directory in {java_file.abs_path}")
+        return ".".join(directories[idx + 1 :]).replace(".java", "")
+
+    @lru_cache
+    def fetch_import_names(self, java_file: JavaFile) -> set[str]:
+        with open(java_file.abs_path, "r") as file:
+            imports: set[str] = set()
+            while line := file.readline():
+                if line.startswith("import"):
+                    imports.add(line.replace("import ", "").replace(";", "").strip())
+                elif "class" in line:
+                    break
+            return imports
+
+    @lru_cache
+    def fetch_links(self, java_file: JavaFile) -> set[SourceFile]:
+        assert all(file.project == java_file.project for file in self._source_files)
+        links: set[SourceFile] = set()
+        for source_file in self._source_files:
+            if self.import_name_of(source_file) in self.fetch_import_names(java_file):
+                links.add(source_file)
+        return links
+
+    def recursive_links(
+        self, target: JavaFile, visited: Optional[set[SourceFile]] = None
+    ) -> set[SourceFile]:
+        if visited is None:
+            visited = set()
+        links: set[SourceFile] = self.fetch_links(target).copy()
+        for link in self.fetch_links(target):
+            if link in visited:
+                continue
+            visited.add(link)
+            links.update(self.recursive_links(link, visited))
+
+        return links
+
+    def graph(self) -> Graph:
+        links = {
+            test_file: self.recursive_links(test_file)
+            for test_file in rich.progress.track(
+                self._test_files, "Creating links for tests..."
+            )
+        }
+        return Graph(
+            source_files=self._source_files, test_files=self._test_files, links=links
+        )
