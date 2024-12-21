edef1b49bf7e91360cd5764265dfe0a97bc517ca
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-12-21 18:58:50 +0300

    :recycle: refactor discriminator

--- src/discriminator.py
+++ src/discriminator.py
@@ -1,51 +1,29 @@
-import json
 import os
 from dataclasses import dataclass
-from typing import NewType, Optional
+from functools import cached_property
+from typing import Protocol
 
 import rich.progress
 
 from src.binder import Repository, SubProject
 from src.binding.file_types import FileName, SourceFile, TestFile
+from src.transaction import TransactionLog, TransactionMap, Transactions
 
-FileNumber = NewType("FileNumber", str)
-
-
-@dataclass(frozen=True)
-class Commit:
-    number: int
-    files: list[FileNumber]
+console = rich.console.Console()
 
 
-def parse_file(file: str) -> list[Commit]:
-    with open(file, "r") as f:
-        return [
-            Commit(number, files=list(map(FileNumber, line.strip().split(" "))))
-            for number, line in enumerate(f)
-        ]
+class Statistics(Protocol):
+    def output(self) -> str: ...
 
 
-def get_mapping(map_file: str) -> dict[FileNumber, list[FileName]]:
-    with open(map_file) as f:
-        return json.load(f)
+class Discriminator(Protocol):
+    transaction: TransactionLog
+    subproject: SubProject
 
+    def __init__(self, transactions: Transactions, subproject: SubProject): ...
 
-@dataclass(frozen=True)
-class Transactions:
-    transactions: list[Commit]
-    mapping: dict[FileNumber, list[FileName]]
-
-    def get_file_number(self, file: FileName) -> Optional[FileNumber]:
-        for k, v in self.mapping.items():
-            if file in v:
-                return k
-        return None
-
-    def first_occurrence(self, file: FileNumber) -> Optional[Commit]:
-        for commit in self.transactions:
-            if file in commit.files:
-                return commit
-        return None
+    @property
+    def statistics(self) -> Statistics: ...
 
 
 @dataclass(frozen=True)
@@ -56,52 +34,41 @@ class TestStatistics:
 
 
 @dataclass(frozen=True)
-class SubProjectStatistics:
+class BeforeAfterStatistics(Statistics):
     test_statistics: list[TestStatistics]
 
-    def output_links(self):
-        for statistic in self.test_statistics:
-            console.print("Test: ", statistic.test.name)
-            console.print(
-                "Before: \n\t->", "\n\t->".join(file.name for file in statistic.before)
-            )
-            console.print(
-                "After: \n\t->", "\n\t->".join(file.name for file in statistic.after)
-            )
-            console.print("[red]=== TERMINUS ===[/red]\n\n")
+    @cached_property
+    def aggregate_before(self) -> set[SourceFile]:
+        return set().union(*[statistic.before for statistic in self.test_statistics])
 
-    def test_first_stats(self) -> tuple[int, int]:
-        test_first: set[SourceFile] = set()
-        test_after: set[SourceFile] = set()
+    @cached_property
+    def aggregate_after(self) -> set[SourceFile]:
+        return set().union(*[statistic.after for statistic in self.test_statistics])
 
-        for statistic in self.test_statistics:
-            test_after.update(statistic.after)
-            test_first.update(statistic.before)
+    @cached_property
+    def test_first(self) -> set[SourceFile]:
+        return self.aggregate_before - self.aggregate_after
 
-        actual_test_first = test_first - test_after
-        return len(actual_test_first), len(test_after)
-
-
-console = rich.console.Console()
+    def output(self) -> str:
+        return f"Test First: {len(self.test_first)}\nTest After: {len(self.aggregate_after)}"
 
 
 @dataclass(frozen=True)
-class SubProjectDiscriminator:
-    transaction: Transactions
+class BeforeAfterDiscriminator(Discriminator):
+    transaction: TransactionLog
     subproject: SubProject
 
     @property
-    def statistics(self) -> SubProjectStatistics:
+    def statistics(self) -> BeforeAfterStatistics:
         output = []
         graph = self.subproject.graph
         for test in rich.progress.track(self.subproject.tests):
             path = FileName(
                 os.path.join(os.path.basename(self.subproject.path), test.path)
             )
-            file_number = self.transaction.get_file_number(path)
-            assert file_number is not None, f"File not found {test.name} @ {path}"
-            base_commit = self.transaction.first_occurrence(file_number)
-            assert base_commit
+            file_number = self.transaction.mapping.name_to_id[path]
+            base_commit = self.transaction.transactions.first_occurrence(file_number)
+            assert base_commit is not None, f"File not found {test.name} @ {path}"
             before, after = [], []
 
             for source_file in graph.links[test]:
@@ -110,11 +77,11 @@ class SubProjectDiscriminator:
                         os.path.basename(self.subproject.path), source_file.path
                     )
                 )
-                file_number = self.transaction.get_file_number(path)
+                file_number = self.transaction.mapping.name_to_id[path]
                 assert (
                     file_number is not None
                 ), f"File not found {source_file.name} @ {path}"
-                commit = self.transaction.first_occurrence(file_number)
+                commit = self.transaction.transactions.first_occurrence(file_number)
                 assert commit
                 if commit.number < base_commit.number:
                     before.append(source_file)
@@ -122,23 +89,18 @@ class SubProjectDiscriminator:
                     after.append(source_file)
             if before or after:
                 output.append(TestStatistics(test, before, after))
-        return SubProjectStatistics(test_statistics=output)
-
-
-@dataclass(frozen=True)
-class Discriminator:
-    transaction: Transactions
-    repository: Repository
+        return BeforeAfterStatistics(test_statistics=output)
 
 
 if __name__ == "__main__":
     repo = Repository(os.path.abspath("../zookeeper"))
     for subproject in repo.subprojects:
-        logs = parse_file("transactions.txt")
-        mapping = get_mapping("mapping.json")
+        with open("transactions.txt") as t, open("mapping.json") as m:
+            logs = Transactions.deserialize(t.read())
+            mapping = TransactionMap.deserialize(m.read())
 
-        transactions = Transactions(logs, mapping)
-        subproject_discriminator = SubProjectDiscriminator(transactions, subproject)
+        transactions = TransactionLog(logs, mapping)
+        subproject_discriminator = BeforeAfterDiscriminator(transactions, subproject)
 
         print(f"Analyzing subproject: {subproject.path}")
-        print(subproject_discriminator.statistics.test_first_stats())
+        print(subproject_discriminator.statistics.output())
