1dba4d81910eb935b3967451466f6089d0e0ece5
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-12-22 14:01:22 +0300

    :truck: move binder.py into _repository

---
+++ src/binding/_repository.py
@@ -0,0 +1,83 @@
+from __future__ import annotations
+
+import os
+from dataclasses import dataclass
+from functools import cached_property
+from typing import Generator, NamedTuple, Type, TypeVar
+
+
+from src.binding.file_types import JavaFile, SourceFile, TestFile
+
+
+MAIN = "main"
+SOURCE_DIR = "src/main/java"
+TEST_DIR = "src/test/java"
+
+T = TypeVar("T", bound=JavaFile)
+
+
+class Files(NamedTuple):
+    source_files: set[SourceFile]
+    test_files: set[TestFile]
+
+
+def _all_files_in_directory(directory: str) -> Generator[str, None, None]:
+    for root, dirnames, files in os.walk(directory):
+        for dir in dirnames:
+            yield from _all_files_in_directory(f"{root}/{dir}")
+        for file in files:
+            if file.endswith(".java"):
+                yield root + os.path.sep + file
+
+
+@dataclass(frozen=True)
+class JavaSubProject:
+    path: str
+
+    @staticmethod
+    def is_project(path: str) -> bool:
+        return all(
+            os.path.exists(f"{path}/{sub_path}") for sub_path in (SOURCE_DIR, TEST_DIR)
+        )
+
+    def _fetch_files_from_directory(
+        self, file_type: Type[T], subdirectory: str
+    ) -> set[T]:
+        return {
+            file_type(
+                project=self.path,
+                path=path.replace(f"{self.path}/", ""),
+            )
+            for path in _all_files_in_directory(f"{self.path}/{subdirectory}")
+        }
+
+    @cached_property
+    def tests(self) -> set[TestFile]:
+        return self._fetch_files_from_directory(TestFile, TEST_DIR)
+
+    @cached_property
+    def source_files(self) -> set[SourceFile]:
+        return self._fetch_files_from_directory(SourceFile, SOURCE_DIR)
+
+
+@dataclass(frozen=True)
+class JavaRepository:
+    root: str
+
+    @cached_property
+    def subprojects(self) -> set[JavaSubProject]:
+        return {
+            JavaSubProject(f"{self.root}/{path}")
+            for path in os.listdir(self.root)
+            if JavaSubProject.is_project(f"{self.root}/{path}")
+        }
+
+    @cached_property
+    def files(self) -> dict[str, Files]:
+        return {
+            project.path: Files(
+                project.source_files,
+                project.tests,
+            )
+            for project in self.subprojects
+        }
