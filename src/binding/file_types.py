0c99aa9092d0e9d34ce0f3d108afbe9cc0e8893a
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-12-22 19:21:03 +0300

    :recycle: remove overrides that are the same

--- src/binding/file_types.py
+++ src/binding/file_types.py
@@ -33,25 +33,13 @@ class ProgramFile:
             return False
         return self.abs_path == other.abs_path
 
-
-@dataclass(frozen=True)
-class SourceFile(ProgramFile):
     def __repr__(self) -> str:
-        return self.name
-
-    def __str__(self) -> str:
         return self.name
 
 
 @dataclass(frozen=True)
-class TestFile(ProgramFile):
-    def __repr__(self) -> str:
-        return self.name
+class SourceFile(ProgramFile): ...
 
-    def __hash__(self) -> int:
-        return hash(self.abs_path)
 
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, ProgramFile):
-            return False
-        return self.abs_path == other.abs_path
+@dataclass(frozen=True)
+class TestFile(ProgramFile): ...
