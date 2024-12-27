bcff4ebb76b4b7ac68f2030bad8eb3c3f83c5a42
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-12-27 20:52:50 +0300

    :sparkles: add diff for graph for easier debugging

--- src/discriminators/binding/graph.py
+++ src/discriminators/binding/graph.py
@@ -15,3 +15,16 @@ class Graph:
         self.source_files.update(other.source_files)
         self.test_files.update(other.test_files)
         self.links.update(other.links)
+
+    def diff(self, other: Graph) -> Graph:
+        source_files = self.source_files.symmetric_difference(other.source_files)
+        test_files = self.test_files.symmetric_difference(other.test_files)
+        links = {
+            k: v.symmetric_difference(other.links[k])
+            for k, v in self.links.items()
+            if k in other.links and v.symmetric_difference(other.links[k])
+        }
+
+        links.update({k: v for k, v in self.links.items() if k not in other.links})
+        links.update({k: v for k, v in other.links.items() if k not in self.links})
+        return Graph(source_files, test_files, links)
