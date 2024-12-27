c06f3facda717d2860418fe33c050c88c49a9407
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2024-12-27 20:50:59 +0300

    :recycle: use field rather make __hash__

--- src/discriminators/binding/file_types.py
+++ src/discriminators/binding/file_types.py
@@ -1,5 +1,5 @@
 import os
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import NewType
 
 FileName = NewType("FileName", str)
@@ -7,8 +7,8 @@ FileName = NewType("FileName", str)
 
 @dataclass(frozen=True)
 class ProgramFile:
-    project: str  # abs to repo/project
-    path: str  # relative to project
+    project: str = field(compare=False, hash=False)  # abs to repo/project
+    path: str = field(compare=True, hash=True)  # relative to project
 
     @property
     def name(self) -> FileName:
@@ -21,14 +21,6 @@ class ProgramFile:
     @property
     def abs_path(self) -> str:
         return os.path.join(self.project, self.path)
-
-    def __hash__(self) -> int:
-        return hash(self.abs_path)
-
-    def __eq__(self, other: object) -> bool:
-        if not isinstance(other, ProgramFile):
-            return False
-        return self.abs_path == other.abs_path
 
     def __repr__(self) -> str:
         return self.name
