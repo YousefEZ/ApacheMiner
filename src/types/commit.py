01dcd444d35f26b60c99fc90398f84f50fc93fdd
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2025-01-02 22:18:05 +0300

    :sparkles: add Commit and ModifiedFiles protocol

---
+++ src/types/commit.py
@@ -0,0 +1,24 @@
+from typing import Optional, Protocol, Sequence
+
+import pydriller
+
+
+class ModifiedFileProtocol(Protocol):
+    @property
+    def change_type(self) -> pydriller.ModificationType: ...
+
+    @property
+    def old_path(self) -> Optional[str]: ...
+
+    @property
+    def new_path(self) -> Optional[str]: ...
+
+
+class CommitProtocol(Protocol):
+    @property
+    def modified_files(
+        self,
+    ) -> Sequence[ModifiedFileProtocol]: ...
+
+    @property
+    def hash(self) -> str: ...
