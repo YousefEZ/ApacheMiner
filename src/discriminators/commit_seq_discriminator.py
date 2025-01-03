17d1acfaf7b311ccd2c24d02eb6d3dfcd303eebe
zcabeto <zcabeto@ucl.ac.uk>
2025-01-03 18:33:07 +0000

    💡 added comments

--- src/discriminators/commit_seq_discriminator.py
+++ src/discriminators/commit_seq_discriminator.py
@@ -22,6 +22,8 @@ class Stats:
 
     @cached_property
     def is_tfd(self) -> bool:
+        """Each time the source is committed, at least one test file updated
+        with new methods that call to the source file"""
         for changed_tests in self.changed_tests_per_commit:
             if len(changed_tests) == 0:
                 return False
@@ -34,12 +36,14 @@ class TestedFirstStatistics(Statistics):
 
     @cached_property
     def test_first(self) -> set[SourceFile]:
+        """Set of source files which are classed as having TFD"""
         return set(
             [statistic.source for statistic in self.test_statistics if statistic.is_tfd]
         )
 
     @cached_property
     def non_test_first(self) -> set[SourceFile]:
+        """Set of source files which are classed as not having TFD"""
         return (
             set([statistic.source for statistic in self.test_statistics])
             - self.test_first
@@ -57,18 +61,22 @@ class CommitSequenceDiscriminator(Discriminator):
     transaction: TransactionLog
     file_binder: BindingStrategy
 
-    def adds_features(self, commit_info) -> bool:
-        if commit_info == ModificationType.ADD:
-            return True
-        if commit_info is None:
+    def adds_features(self, file_commit_info) -> bool:
+        """Does this commit add new methods to the file?"""
+        if file_commit_info == ModificationType.ADD:
+            return True  # auto-accept file creations
+        if file_commit_info is None:
             return False  # no change
-        if not isinstance(commit_info, tuple):
+        if not isinstance(file_commit_info, tuple):
             return False  # not a modification with method additions
-        if len(commit_info[1]) == 0:
+
+        _, added_methods, _ = file_commit_info
+        if len(added_methods) == 0:
             return False  # no methods added
         return True
 
     def next_commit(self, file_number, commits: list[Commit]) -> Optional[Commit]:
+        """Find the next commit which modifies the file with a feature addition"""
         for commit in commits:
             if file_number in commit.files and self.adds_features(
                 commit.files[file_number]
@@ -82,6 +90,8 @@ class CommitSequenceDiscriminator(Discriminator):
         tests: set[TestFile],
         source_name: str,
     ) -> dict[TestFile, list[int]]:
+        """Within the range of commits, find the test files which are updated
+        with new methods that call to the source file"""
         hits: dict[TestFile, list[int]] = defaultdict(list)
         for commit in self.transaction.transactions.commits[range[0] : range[1] + 1]:
             for test_file in tests:
@@ -93,7 +103,8 @@ class CommitSequenceDiscriminator(Discriminator):
                     commit.files[test_id]
                 ):
                     if isinstance(commit.files[test_id], tuple):
-                        if source_name in commit.files[test_id][2]:
+                        _, _, class_calls = commit.files[test_id]
+                        if source_name in class_calls:
                             hits[test_file].append(commit.number)
                             # test file updated with new methods and calls to source
                     else:
@@ -103,11 +114,14 @@ class CommitSequenceDiscriminator(Discriminator):
 
     @property
     def statistics(self) -> TestedFirstStatistics:
+        """Get set of every source file feature addition which are tested first"""
         output = []
         graph = self.file_binder.graph()
+        commit_count = len(self.transaction.transactions.commits)
         for source_file in rich.progress.track(graph.source_files):
+            # setup the source file for tracking
             if source_file not in graph.source_to_test_links:
-                continue  # no tests for this source file
+                continue
             path = FileName(
                 os.path.join(os.path.basename(source_file.project), source_file.path)
             )
@@ -118,21 +132,25 @@ class CommitSequenceDiscriminator(Discriminator):
                 self.transaction.transactions.first_occurrence(source_id)
             )
             if this_commit is None:
-                continue  # never appears in the transaction log
+                continue
+
+            # until the file is deleted or the last commit is reached
             while (
                 this_commit is not None
                 and this_commit.files[source_id] != ModificationType.DELETE
-                and last_commit.number <= len(self.transaction.transactions.commits) - 1
+                and last_commit.number <= commit_count - 1
             ):
+                # find test files updated with new methods calling to the source file
                 hits = self.tfd_iterations(
-                    (last_commit.number, this_commit.number),
-                    graph.source_to_test_links[source_file],
-                    source_file.name.replace(".java", ""),
+                    range=(last_commit.number, this_commit.number),
+                    tests=graph.source_to_test_links[source_file],
+                    source_name=source_file.name.replace(".java", ""),
                 )
                 stats.changed_tests_per_commit.append(hits)
-                if this_commit.number == len(self.transaction.transactions.commits) - 1:
+
+                # setup next iteration with the next time this source file is committed
+                if this_commit.number == commit_count - 1:
                     break
-                # find next commit with source file
                 last_commit = self.transaction.transactions.commits[
                     this_commit.number + 1
                 ]
