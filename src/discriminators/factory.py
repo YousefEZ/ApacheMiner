f94d02f69648340e3286bd3c90839b0e58199b56
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2025-01-08 20:12:54 +0300

    :sparkles: add branch discriminator to factory

--- src/discriminators/factory.py
+++ src/discriminators/factory.py
@@ -4,12 +4,14 @@ from src.discriminators.before_after_discriminator import BeforeAfterDiscriminat
 from src.discriminators.commit_seq_discriminator import CommitSequenceDiscriminator
 from src.discriminators.discriminator import Discriminator
 from src.discriminators.LLM_discriminator import LLMDiscriminator
+from src.discriminators.branch_discriminator import BranchDiscriminator
 
-DiscriminatorTypes = Literal["before_after", "commit_sequence", "llm"]
+DiscriminatorTypes = Literal["before_after", "commit_sequence", "llm", "branch"]
 
 
 discriminator_factory: dict[DiscriminatorTypes, Type[Discriminator]] = {
     "before_after": BeforeAfterDiscriminator,
     "commit_sequence": CommitSequenceDiscriminator,
     "llm": LLMDiscriminator,
+    "branch": BranchDiscriminator,
 }
