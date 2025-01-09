7ae567b5d3dd0238d5e5e0f672585648fc1830c1
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2025-01-10 00:02:16 +0300

    :truck: move JavaRepository to java.py

---
+++ src/discriminators/binding/repositories/java.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import Type, override
+
+from src.discriminators.binding.file_types import ProgramFile
+from src.discriminators.binding.repositories.repository import Repository
+from src.discriminators.binding.repositories.languages.language import Language
+from src.discriminators.binding.repositories.languages.java import JavaLanguage
+
+
+@dataclass(frozen=True)
+class JavaRepository(Repository):
+    @property
+    @override
+    def language(self) -> Type[Language]:
+        return JavaLanguage
+
+    @override
+    def is_test(self, file: ProgramFile) -> bool:
+        for line in file.get_source_code():
+            if "@Test" in line:
+                return True
+        return False
